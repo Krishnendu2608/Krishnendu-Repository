dcd3ac2c708cdd7fc396170a9d788c119d1af741
Krishnendu Saha <krishnendu5588@gmail.com>
2026-07-15 21:22:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (70.16%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def search(self, nums, target):
+        left=0
+        right=len(nums)-1
+        while left<=right:
+            mid_index=(left+right)//2
+            mid_number=nums[mid_index]
+            if target==mid_number:
+                return mid_index
+            elif target<mid_number:
+                right=(mid_index)-1
+            elif target>mid_number:
+                left=(mid_index) + 1
+        return -1
+        
