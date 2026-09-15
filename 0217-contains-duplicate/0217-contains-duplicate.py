f1e254f5b445c9acd90b4b0ac0c1ac0e04de2833
Krishnendu Saha <krishnendu5588@gmail.com>
2026-09-15 20:05:42 +0530

    Time: 99 ms (5.22%), Space: 32.7 MB (7.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,11 +3,11 @@ class Solution(object):
         frq={}
         for i in nums:
             if i in frq:
-                frq[i]+=1
-            elif i not in frq:
+                frq[i] +=1
+            else:
                 frq[i]=1
-        for key,count in frq.items():
-            if count>1:
+        for key,values in frq.items():
+            if values >1:
                 return True
         return False
-               
+        
