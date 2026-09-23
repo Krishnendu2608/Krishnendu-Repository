2281ad86892463ff23aa782a7de1c6b34bfb4219
Krishnendu Saha <krishnendu5588@gmail.com>
2026-09-23 20:59:34 +0530

    Time: 0 ms (100%), Space: 12.3 MB (57.67%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,14 +1,7 @@
 class Solution(object):
     def checkIfPangram(self, sentence):
-        frq_map={}
-        for i in sentence:
-            if i not in frq_map:
-                frq_map[i]=1
-            elif i in frq_map:
-                frq_map[i] +=1
-        map_length=len(frq_map)
-        if map_length==26:
+        if len(set(sentence))==26:
             return True
         else:
             return False
-                
+        
