29b8807b2c38558930b55f52ce8ac6079a3f40a0
Krishnendu Saha <krishnendu5588@gmail.com>
2026-09-25 09:40:56 +0530

    Time: 312 ms (60.77%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,7 @@
 select name,population,area
 from World
-where area>=3000000 or population>=25000000;
+where area>=3000000 
+union
+select name,population,area
+from World
+where population>=25000000 
